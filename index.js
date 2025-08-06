8256e272ec178eee725651e140e95bd6524c9821
AdzhanRE <muhdadzhan@gmail.com>
2025-08-06 13:15:18 +0800

    Add cors

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const app = express();
 const userRoutes = require('./routes/userRoutes');
@@ -10,6 +11,11 @@ const port = process.env.PORT || process.env.PORT;
 
 app.use(express.json()); //for raw json
 app.use(express.urlencoded({extended: true}));
+
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 
 app.use('/api', userRoutes);
 app.use('/api', studentRoutes);
